f50026059c0c5f0e1e764c93caa024d2ae680d23
JCCotrina <juan.cotrina.m@uni.pe>
2022-09-08 11:00:38 -0500

    add the mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuFriesIcon = document.querySelector('.menu');
 
 navBarEmail.addEventListener("click", toggleDesktopMenu);
+menuFriesIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
